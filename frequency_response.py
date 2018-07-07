218cff14fb60444829f26757223b38c58a0f8f5f
jaakko <jaakko.o.pasanen@gmail.com>
2018-07-07 18:34:21 +0300

    Added calibrated data to FrequencyResponse. From now on raw data is the uncalibrated (uncompensated) data measured by microphone. Calibration is subtracted from raw, not added. calibrate and center method adjust other datas too not just raw. Not plotting non-smoothed data if smoothed is available.

--- frequency_response.py
+++ frequency_response.py
@@ -20,6 +20,7 @@ class FrequencyResponse:
                  name=None,
                  frequency=None,
                  raw=None,
+                 calibrated=None,
                  smoothed=None,
                  equalization=None,
                  equalized_raw=None,
@@ -33,6 +34,10 @@ class FrequencyResponse:
         self.raw = raw if raw is not None else []
         self.raw = [None if x is None or math.isnan(x) or x is None else x for x in self.raw]
         self.raw = np.array(self.raw)
+
+        self.calibrated = calibrated if calibrated is not None else []
+        self.calibrated = [None if x is None or math.isnan(x) or x is None else x for x in self.calibrated]
+        self.calibrated = np.array(self.calibrated)
 
         self.smoothed = smoothed if smoothed is not None else []
         self.smoothed = np.array(self.smoothed)
@@ -60,6 +65,8 @@ class FrequencyResponse:
         self.frequency = self.frequency[sorted_inds]
         if len(self.raw):
             self.raw = self.raw[sorted_inds]
+        if len(self.calibrated):
+            self.calibrated = self.calibrated[sorted_inds]
         if len(self.smoothed):
             self.smoothed = self.smoothed[sorted_inds]
         if len(self.equalization):
@@ -78,6 +85,7 @@ class FrequencyResponse:
         df = pd.read_csv(file_path, sep=',', header=0)
         frequency = list(df['frequency']) if 'frequency' in df else None
         raw = list(df['raw']) if 'raw' in df else None
+        calibrated = list(df['calibrated']) if 'calibrated' in df else None
         smoothed = list(df['smoothed']) if 'smoothed' in df else None
         equalization = list(df['equalization']) if 'equalization' in df else None
         equalized_raw = list(df['equalized_raw']) if 'equalized_raw' in df else None
@@ -87,6 +95,7 @@ class FrequencyResponse:
             name=name,
             frequency=frequency,
             raw=raw,
+            calibrated=calibrated,
             smoothed=smoothed,
             equalization=equalization,
             equalized_raw=equalized_raw,
@@ -96,12 +105,13 @@ class FrequencyResponse:
     def write_to_csv(self, file_path=None):
         """Writes data to files as CSV."""
         file_path = os.path.abspath(file_path)
-
         df = pd.DataFrame()
         if len(self.frequency):
             df['frequency'] = self.frequency
         if len(self.raw):
             df['raw'] = [x if x is not None else 'NaN' for x in self.raw]
+        if len(self.calibrated):
+            df['calibrated'] = [x if x is not None else 'NaN' for x in self.calibrated]
         if len(self.smoothed):
             df['smoothed'] = [x if x is not None else 'NaN' for x in self.smoothed]
         if len(self.equalization):
@@ -110,7 +120,6 @@ class FrequencyResponse:
             df['equalized_raw'] = [x if x is not None else 'NaN' for x in self.equalized_raw]
         if len(self.equalized_smoothed):
             df['equalized_smoothed'] = [x if x is not None else 'NaN' for x in self.equalized_smoothed]
-
         df.to_csv(file_path, header=True, index=False)
 
     def write_eqapo_graphic_eq(self, file_path):
@@ -129,22 +138,22 @@ class FrequencyResponse:
         return s
 
     @staticmethod
-    def generate_frequencies(f_min=10, f_max=30000, step=1.01):
+    def generate_frequencies(f_min=10, f_max=30000, f_step=1.01):
         freq_new = []
         # Frequencies from 20kHz down
         f = np.min([20000, f_max])
         while f > f_min:
-            freq_new.append(round(f))
-            f = f / step
+            freq_new.append(int(round(f)))
+            f = f / f_step
         # Frequencies from 20kHZ up
         f = np.min([20000, f_max])
         while f < f_max:
-            freq_new.append(round(f))
-            f = f * step
+            freq_new.append(int(round(f)))
+            f = f * f_step
         freq_new = sorted(set(freq_new))  # Remove duplicates and sort ascending
         return np.array(freq_new)
 
-    def interpolate(self, f=None, step=1.01, pol_order=1, f_min=10, f_max=30000):
+    def interpolate(self, f=None, f_step=1.01, pol_order=1, f_min=10, f_max=30000):
         """Interpolates missing values from previous and next value."""
         # Remove None values
         i = 0
@@ -157,7 +166,7 @@ class FrequencyResponse:
         interpolator = InterpolatedUnivariateSpline(np.log10(self.frequency), self.raw, k=pol_order)
 
         if f is None:
-            self.frequency = self.generate_frequencies(f_min=f_min, f_max=f_max, step=step)
+            self.frequency = self.generate_frequencies(f_min=f_min, f_max=f_max, f_step=f_step)
         else:
             self.frequency = f
         self.raw = interpolator(np.log10(self.frequency))
@@ -165,7 +174,13 @@ class FrequencyResponse:
     def center(self):
         """Removed bias from frequency response."""
         interpolator = InterpolatedUnivariateSpline(np.log10(self.frequency), self.raw, k=1)
-        self.raw -= interpolator(np.log10(1000))
+        diff = interpolator(np.log10(1000))
+        self.raw -= diff
+        self.calibrated -= diff
+        self.smoothed -= diff
+        self.equalization -= diff
+        self.equalized_raw -= diff
+        self.equalized_smoothed -= diff
 
     def _window_size(self, octaves):
         """Calculates moving average window size in indices from octaves."""
@@ -187,13 +202,13 @@ class FrequencyResponse:
             window_size += 1
         return window_size
 
-    def _sigmoid(self, f_lower, f_upper, g_normal=0, g_treble=1):
+    def _sigmoid(self, f_lower, f_upper, a_normal=0, a_treble=1):
         f_center = np.sqrt(f_upper / f_lower) * f_lower
         half_range = np.log10(f_upper) - np.log10(f_center)
         f_center = np.log10(f_center)
-        g = expit((np.log10(self.frequency) - f_center) / (half_range / 4))
-        g = g * -(g_normal - g_treble) + g_normal
-        return g
+        a = expit((np.log10(self.frequency) - f_center) / (half_range / 4))
+        a = a * -(a_normal - a_treble) + a_normal
+        return a
 
     def smooth(self,
                window_size=1/5,
@@ -214,10 +229,6 @@ class FrequencyResponse:
             treble_f_upper: Upper boundary of transition frequency reqion. In the transition region normal filter is \
                         switched to treble filter with sigmoid weighting function.
         """
-        if None in self.frequency or None in self.raw or None in self.equalization or None in self.equalized_raw:
-            # Must not contain None values
-            raise ValueError('None values present, cannot smoothen!')
-
         if treble_f_upper <= treble_f_lower:
             raise ValueError('Upper transition boundary must be greater than lower boundary')
 
@@ -227,13 +238,20 @@ class FrequencyResponse:
         if treble_iterations is None:
             treble_iterations = iterations
 
+        # Use calibrated data if available
+        data = self.calibrated if len(self.calibrated) else self.raw
+
+        if None in self.frequency or None in data:
+            # Must not contain None values
+            raise ValueError('None values present, cannot smoothen!')
+
         # Normal filter
-        y_normal = self.raw
+        y_normal = data
         for _ in range(iterations):
             y_normal = savgol_filter(y_normal, self._window_size(window_size), 2)
 
         # Treble filter
-        y_treble = self.raw
+        y_treble = data
         for _ in range(treble_iterations):
             y_treble = savgol_filter(y_treble, self._window_size(treble_window_size), 2)
 
@@ -244,7 +262,15 @@ class FrequencyResponse:
 
     def calibrate(self, calibration):
         """Calibrates raw frequency response data with calibration array."""
-        self.raw += calibration.raw
+        calibrated_new = self.raw - calibration.raw
+        if len(self.calibrated):
+            # Already calibrated, calculate difference between new and old calibration
+            diff = calibrated_new - self.calibrated
+            self.smoothed -= diff
+            self.equalization -= diff
+            self.equalized_raw -= diff
+            self.equalized_smoothed -= diff
+        self.calibrated = calibrated_new
 
     def _target(self, bass_boost=4):
         """Creates target curve with bass boost as described by harman target response.
@@ -288,21 +314,23 @@ class FrequencyResponse:
         self.equalization = []
         self.equalized_raw = []
 
-        data = self.smoothed if len(self.smoothed) else self.raw
+        if len(self.smoothed):
+            data = self.smoothed
+        elif len(self.calibrated):
+            data = self.calibrated
+        else:
+            data = self.raw
 
         if None in data or None in self.equalization or None in self.equalized_raw:
             # Must not contain None values
             self.interpolate()
-
-        #if smooth:
-        #    max_gain *= 0.9
 
         # Invert with max gain clipping
         previous_clipped = False
         kink_inds = []
         self.target = self._target(bass_boost=bass_target)
         # Max gain at each frequency
-        max_gain = self._sigmoid(treble_f_lower, treble_f_upper, g_normal=max_gain, g_treble=treble_max_gain)
+        max_gain = self._sigmoid(treble_f_lower, treble_f_upper, a_normal=max_gain, a_treble=treble_max_gain)
         for i, a in enumerate(data):
             gain = self.target[i] - a
             clipped = gain > max_gain[i]
@@ -349,10 +377,10 @@ class FrequencyResponse:
                    ax=None,
                    show=True,
                    raw=True,
+                   calibrated=True,
                    smoothed=True,
                    equalization=True,
                    equalized=True,
-                   rounded=True,
                    target=True,
                    file_path=None,
                    f_min=10,
@@ -375,13 +403,16 @@ class FrequencyResponse:
         if raw and len(self.raw):
             plt.plot(self.frequency, self.raw, linewidth=1)
             legend.append('Raw')
+        if calibrated and len(self.calibrated) and not (smoothed and len(self.smoothed)):
+            plt.plot(self.frequency, self.calibrated, linewidth=1)
+            legend.append('Calibrated')
         if smoothed and len(self.smoothed):
             plt.plot(self.frequency, self.smoothed, linewidth=1)
-            legend.append('Smoothed')
+            legend.append('Calibrated and Smoothed')
         if equalization and len(self.equalization):
             plt.plot(self.frequency, self.equalization, linewidth=1)
             legend.append('Equalization')
-        if equalized and len(self.equalized_raw):
+        if equalized and len(self.equalized_raw) and not len(self.equalized_smoothed):
             plt.plot(self.frequency, self.equalized_raw, linewidth=1)
             legend.append('Equalized raw')
         if equalized and len(self.equalized_smoothed):
@@ -392,9 +423,13 @@ class FrequencyResponse:
         plt.semilogx()
         plt.xlim([f_min, f_max])
         plt.ylabel('Amplitude (dBr)')
+        if a_min is None:
+            a_min = max(np.min(self.raw) - 10, -40)
+        if a_max is None:
+            a_max = min(np.max(self.raw) + 10, 20)
         plt.ylim([a_min, a_max])
         plt.title(self.name)
-        plt.legend(legend, loc='upper right', fontsize=8)
+        plt.legend(legend, fontsize=8)
         plt.grid(True, which='major')
         plt.grid(True, which='minor')
         ax.xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:.0f}'))
@@ -452,6 +487,7 @@ class FrequencyResponse:
             # Creates FrequencyReponse for calibration data
             file_path = os.path.abspath(calibration)
             calibration = FrequencyResponse.read_from_csv(file_path)
+            calibration.interpolate()
 
         # Dir paths to absolute
         input_dir = os.path.abspath(input_dir)
@@ -465,9 +501,7 @@ class FrequencyResponse:
 
             # Copy relative path to output directory
             relative_path = os.path.relpath(file_path, input_dir)
-            print(relative_path)
             file_path = os.path.join(output_dir, relative_path)
-            print(file_path)
             dir_path = os.path.dirname(file_path)
             if not os.path.isdir(dir_path):
                 os.makedirs(dir_path, exist_ok=True)
@@ -475,12 +509,12 @@ class FrequencyResponse:
             # Interpolate to standard frequency vector
             fr.interpolate()
 
+            # Center by 1kHz
+            fr.center()
+
             if calibration is not None:
                 # Calibrate
                 fr.calibrate(calibration)
-
-            # Center by 1kHz
-            fr.center()
 
             # Smooth data
             fr.smooth(
@@ -506,7 +540,12 @@ class FrequencyResponse:
             # Write results to CSV file
             fr.write_to_csv(file_path)
             # Write plots to file and optionally display them
-            fig, ax = fr.plot_graph(show=show_plot, file_path=file_path.replace('.csv', '.png'))
+            fig, ax = fr.plot_graph(
+                show=show_plot,
+                file_path=file_path.replace('.csv', '.png'),
+                #equalization=False,
+                #equalized=False
+            )
             plt.close(fig)
             # Write EqualizerAPO settings to file
             _eq_apo_str = fr.write_eqapo_graphic_eq(file_path.replace('.csv', ' EqAPO.txt'))
